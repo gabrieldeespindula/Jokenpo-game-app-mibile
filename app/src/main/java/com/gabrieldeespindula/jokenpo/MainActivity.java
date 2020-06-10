ef8cd4def0d6aa0adfd78b617fc7b73267cf8cf7
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-10 19:32:20 -0300

    Implementing View.OnClickListener.

--- app/src/main/java/com/gabrieldeespindula/jokenpo/MainActivity.java
+++ app/src/main/java/com/gabrieldeespindula/jokenpo/MainActivity.java
@@ -2,6 +2,7 @@ package com.gabrieldeespindula.jokenpo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.kww = findViewById(R.id.kww_button);
         this.mViewHolder.infinite = findViewById(R.id.infinite_button);
         this.mViewHolder.mode = findViewById(R.id.mode_button);
+
+        this.mViewHolder.classic.setOnClickListener(this);
+        this.mViewHolder.kww.setOnClickListener(this);
+        this.mViewHolder.infinite.setOnClickListener(this);
+        this.mViewHolder.mode.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 
