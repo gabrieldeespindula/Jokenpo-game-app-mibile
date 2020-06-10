87e7c2635336f43150b09751211bd097fb381955
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-10 19:28:41 -0300

    Mapping the buttons.

--- app/src/main/java/com/gabrieldeespindula/jokenpo/MainActivity.java
+++ app/src/main/java/com/gabrieldeespindula/jokenpo/MainActivity.java
@@ -2,12 +2,28 @@ package com.gabrieldeespindula.jokenpo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.classic = findViewById(R.id.classic_button);
+        this.mViewHolder.kww = findViewById(R.id.kww_button);
+        this.mViewHolder.infinite = findViewById(R.id.infinite_button);
+        this.mViewHolder.mode = findViewById(R.id.mode_button);
+
+    }
+
+    private static class ViewHolder {
+        Button classic;
+        Button kww;
+        Button infinite;
+        Button mode;
     }
 }
