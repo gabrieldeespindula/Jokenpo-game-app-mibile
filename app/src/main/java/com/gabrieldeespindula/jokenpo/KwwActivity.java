6ab9e0103b3dedec7870b3cde75d102be22b3051
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-12 09:50:54 -0300

    Implementing View.OnClickListening on KwwActivity.java

--- app/src/main/java/com/gabrieldeespindula/jokenpo/KwwActivity.java
+++ app/src/main/java/com/gabrieldeespindula/jokenpo/KwwActivity.java
@@ -2,11 +2,12 @@ package com.gabrieldeespindula.jokenpo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class KwwActivity extends AppCompatActivity {
+public class KwwActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -22,6 +23,15 @@ public class KwwActivity extends AppCompatActivity {
         this.mViewHolder.stone = findViewById(R.id.stone_button);
         this.mViewHolder.paper = findViewById(R.id.paper_button);
         this.mViewHolder.scissors = findViewById(R.id.scissors_button);
+
+        this.mViewHolder.stone.setOnClickListener(this);
+        this.mViewHolder.paper.setOnClickListener(this);
+        this.mViewHolder.scissors.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 
